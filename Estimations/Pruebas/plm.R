ad3a17f3c7702de8ac5695a923d3cbd65484e2f9
aspardog <92935386+aspardog@users.noreply.github.com>
2023-02-13 02:59:44 -0800

    Primera entrega

--- Estimations/Pruebas/plm.R
+++ Estimations/Pruebas/plm.R
@@ -1,21 +1,53 @@
-library(tidyverse, plm)
+library(tidyverse)
+library(plm)
+library(sandwich)
+library(lmtest)
+
 correlaciones.df <- merge_data %>%
   select(starts_with("ruv"), spraying)
 
 a <- cor(correlaciones.df)
 
+merge_data <- merge_data %>%
+  mutate(codmpio = as.factor(codmpio),
+         year = as.factor(year),
+         dpto = as.factor(dpto),
+         month = as.factor(month))
 controles <- c("ruv_amenaza", "ruv_combates", "ruv_desaparicion_forzada", "ruv_homicidio", 
-"ruv_lesiones_fis", "ruv_lesiones_psico","ruv_minas", "ruv_tortura", "ruv_violencia_sexual", "ruv_abandono_despojo")
+"ruv_lesiones_fis", "ruv_lesiones_psico","ruv_minas", "ruv_tortura", "ruv_violencia_sexual", 
+"ruv_abandono_despojo", "year", "dpto")
 
-reg <- lm(data = merge_data, formula = paste0("ruv_desplazamiento_forzado ~ spraying + ", paste(controles, collapse = "+")))
+reg <- lm(data = merge_data, 
+          formula = paste0("ruv_desplazamiento_forzado ~ spraying + ", paste(controles, collapse = "+")))
 summary(reg)
+m1coeffs_std <- data.frame(summary(reg)$coefficients)
+coi_indices <- which(!startsWith(row.names(m1coeffs_std), 'dpto'))
+m1coeffs_std[coi_indices,]
+
+m1coeffs_cl <- coeftest(reg, vcov = vcovCL, cluster = ~codmpio)
+m1coeffs_cl[coi_indices,]
+(m1cis <- coefci(reg, parm = coi_indices, vcov = vcovCL,
+                cluster = ~codmpio, level = 0.9))
+
+
+# Second prueba
+merge_data <- merge_data %>%
+  mutate(fe = paste0(dpto, year))
+
 
 fixed_effects <- plm::plm(paste0("ruv_desplazamiento_forzado ~ spraying + ", paste(controles, collapse = "+")),
                      data = merge_data, 
-                     index = c("codmpio","year"),
+                     index = c("year"),
                      model = "within",
                      effect = "twoways")
 summary(fixed_effects)
+
+vcov <- fixed_effects$vcov
+
+m1coeffs_cl <- coeftest(fixed_effects, vcov = vcov, cluster = ~codmpio)
+m1coeffs_cl[coi_indices,]
+
+summary(m1coeffs_cl)
 
 # Clusterizar errores estandar a nivel de municipio
 # Libreria sandwich 
---
+++ Visualizations/src/aspersiones.R
@@ -0,0 +1,98 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## Script:            Tesis Maestria
+##
+## Author(s):        Santiago Pardo        (santiagopardo03@gmail.com)
+##
+## Dependencies:      Universidad de los Andes
+##
+## Creation date:     February 12th, 2023
+##
+## This version:      February 12th, 2023
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## Visualizations                                                                                       ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+##    Set - UP                                                                                             ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+library(pacman)
+p_load(sf, tmap, RColorBrewer, cartogram, geodaData, tidyverse, ggthemes)
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+##   1. Dowloading and Processing data                                                                      ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Load data
+merge_data.df <- readRDS("Data/Merge/output/merge_data.rds") %>%
+  filter(year < 2013) %>%
+  filter(year > 2003)
+  
+
+colombia.sf <- st_read("Data/Download/input/ShapeFiles/Municipio_ctm12.shp") %>%
+  mutate(codmpio = as.numeric(mpio_ccnct)) %>%
+  filter(codmpio != 88001) %>%
+  filter(codmpio != 88564) 
+
+merge_data.sf <- colombia.sf %>%
+  left_join(merge_data.df, by = "codmpio") %>%
+  select(codmpio, spraying) %>%
+  mutate(aspersiones = if_else(spraying == 0 , NA_real_, spraying),
+         aspersiones = round(aspersiones, 0))
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+##    2. Graph                                                                          ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+mapas <- function(data,variable, grupo){
+  
+  mapa <- data %>%
+    group_by({{grupo}}) %>%
+    dplyr::summarize(counter = sum({{variable}}, na.rm = T))
+  
+  mapa <- mapa %>%
+    mutate(breaks = cut(counter, b = unique((quantile(counter, probs= seq(0,1,1/20), na.rm = T))), dig.lab = 7), include.lowest = TRUE) %>%
+    arrange(counter)
+  
+  col <- 5
+  fichas <- colorRampPalette(brewer.pal(9, "BuGn"))(col)
+  
+  breaks2 <- unique(pull(mapa,breaks))
+  
+  breaks2 <- na.omit(breaks2)
+  
+  a <- ggplot(mapa) +
+    geom_sf(aes(fill = breaks),color ="black",size = 0.1)+
+    scale_fill_manual(values = fichas,
+                      limits = breaks2, na.value = "white") +
+    theme_map()+
+    labs(fill = "Hectáreas asperjadas",
+         caption = "Fuente: DIRAN") +
+    theme(legend.key.size = unit(1, "cm"),
+          legend.position = "right",
+          legend.key = element_blank(),
+          legend.background = element_blank(),
+          axis.line = element_blank(),
+          legend.title=element_text(size=12,face="bold"), 
+          legend.text=element_text(size=10),
+          plot.title=element_text(size=14, hjust=0.8, vjust=1, face="bold"),
+          plot.subtitle=element_text(size=10, hjust=0.6, vjust=1),
+          plot.caption=element_text(size=8, hjust=0.01))
+  
+  return(a)
+}
+a <- mapas(data = merge_data.sf, variable = aspersiones, grupo = codmpio)
+
+### PLOT
+
+ggsave(a, filename = "Visualizations/output/aspersiones.png", dpi = 320, width = 10, height = 10)
+
+
