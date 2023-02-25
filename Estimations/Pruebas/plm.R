60795ce48f0bee97971d2119e2e80c8ad1956ab7
aspardog <92935386+aspardog@users.noreply.github.com>
2023-02-25 14:49:14 -0600

    Julita

--- Estimations/Pruebas/plm.R
+++ Estimations/Pruebas/plm.R
@@ -2,23 +2,30 @@ library(tidyverse)
 library(plm)
 library(sandwich)
 library(lmtest)
+library(ivreg)
 
-correlaciones.df <- merge_data %>%
-  select(starts_with("ruv"), spraying)
+
+correlaciones.df <- merge_data.df %>%
+  select(starts_with("ruv"), spraying, starts_with("cnmh"), night_lights)
 
 a <- cor(correlaciones.df)
 
-merge_data <- merge_data %>%
+merge_data <- merge_data.df %>%
+  filter(year > 2003 & year < 2013) %>%
+  mutate(ydep = log(ruv_desplazamiento_forzado)) %>%
   mutate(codmpio = as.factor(codmpio),
          year = as.factor(year),
          dpto = as.factor(dpto),
          month = as.factor(month))
-controles <- c("ruv_amenaza", "ruv_combates", "ruv_desaparicion_forzada", "ruv_homicidio", 
-"ruv_lesiones_fis", "ruv_lesiones_psico","ruv_minas", "ruv_tortura", "ruv_violencia_sexual", 
-"ruv_abandono_despojo", "year", "dpto")
+controles <- c("ruv_amenaza", "ruv_desaparicion_forzada",
+               "ruv_minas", "ruv_tortura",  "cnmh_violencia_sexual",
+               "ruv_lesiones_fis", "ruv_lesiones_psico", "ruv_abandono_despojo", 
+               "cnmh_atentado","cnmh_masacres", "cnmh_asesinatos_selectivos",
+               "cnmh_enfrentamientos", "night_lights", "year", "codmpio")
 
 reg <- lm(data = merge_data, 
-          formula = paste0("ruv_desplazamiento_forzado ~ spraying + ", paste(controles, collapse = "+")))
+          formula = paste0("log(spraying + quantile(spraying, .25)^2/quantile(spraying, .75)) ~  windIV20MERRA +", paste(controles, collapse = "+")))
+
 summary(reg)
 m1coeffs_std <- data.frame(summary(reg)$coefficients)
 coi_indices <- which(!startsWith(row.names(m1coeffs_std), 'dpto'))
@@ -35,7 +42,7 @@ merge_data <- merge_data %>%
   mutate(fe = paste0(dpto, year))
 
 
-fixed_effects <- plm::plm(paste0("ruv_desplazamiento_forzado ~ spraying + ", paste(controles, collapse = "+")),
+fixed_effects <- plm::plm(paste0("log(ruv_desplazamiento_forzado + min(ruv_desplazamiento_forzado[ruv_desplazamiento_forzado > 0])/2) ~ windSpeedFLDAS + ", paste(controles, collapse = "+")),
                      data = merge_data, 
                      index = c("year"),
                      model = "within",
@@ -52,6 +59,10 @@ summary(m1coeffs_cl)
 # Clusterizar errores estandar a nivel de municipio
 # Libreria sandwich 
 # Efectos fijos, ajustar errores estandar para clusterizar a nivel municipal
+prueba <- lm(formula = ruv_desplazamiento_forzado ~ windIV20MERRA, data = merge_data)
+instrumento <- lm(formula = spraying ~  windIV20MERRA, data = merge_data)
+summary(instrumento)
 
-
-
+a <- ivreg::ivreg(formula = paste0("log(ruv_desplazamiento_forzado + quantile(ruv_desplazamiento_forzado, .25)^2/quantile(ruv_desplazamiento_forzado, .75)) ~ spraying | windIV20MERRA + windSpeedFLDAS +", paste(controles, collapse = "+")), 
+                  data = merge_data)
+summary(a)
---
+++ Estimations/src/regressions.R
@@ -0,0 +1,117 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## Script:            Tesis Maestria
+##
+## Author(s):        Santiago Pardo        (santiagopardo03@gmail.com)
+##
+## Dependencies:      Universidad de los Andes
+##
+## Creation date:     February 2nd, 2023
+##
+## This version:      February 2nd, 2023
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+##  Model Estimation                                                                                 ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+##    Set - UP                                                                                             ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+library(pacman)
+p_load(tidyverse, sandwich, lmtest, ivreg, corrr)
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+##   1. Download                                                                                            ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+merge_data.df <- readRDS("Data/Merge/output/merge_data.rds") %>%
+  filter(year < 2013 & year > 2003) %>%
+  mutate(codmpio = as.factor(codmpio),
+         year = as.factor(year),
+         dpto = as.factor(dpto),
+         month = as.factor(month))
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+##   1. Correlations                                                                                        ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+controles <- c("ruv_amenaza", "ruv_desaparicion_forzada", "ruv_combates", "ruv_secuestro",
+               "ruv_minas", "ruv_tortura", "ruv_homicidio", "ruv_reclutamiento_menores", "cnmh_violencia_sexual",
+               "ruv_abandono_despojo", "ruv_perdida_bienes", "cnmh_masacres", 
+               "night_lights")
+controles_fe <- c("ruv_amenaza", "ruv_desaparicion_forzada", "ruv_combates", "ruv_secuestro",
+               "ruv_minas", "ruv_tortura", "ruv_reclutamiento_menores", "cnmh_violencia_sexual",
+               "ruv_abandono_despojo", "ruv_perdida_bienes", "cnmh_masacres", 
+               "night_lights", "codmpio", "year")
+
+correlaciones.df <- merge_data.df %>%
+  select(starts_with(controles), spraying, night_lights, windIV20MERRA, windSpeedFLDAS)
+
+correlaciones <- correlate(x = correlaciones.df[,-1]) 
+
+# Homicidio (RUV), Reclutamiento (RUV), Amenazas (RUV), Desaparición (RUV) and Despojo (RUV) are the variables with the highest correlation
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+##   2. Fixed Effects Regression                                                                             ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+lm.reg <- lm(data = merge_data.df, 
+             formula = paste0("log(ruv_desplazamiento_forzado + quantile(ruv_desplazamiento_forzado, .25)^2/quantile(ruv_desplazamiento_forzado, .75)) ~  spraying"))
+summary(lm.reg)
+
+fe.reg <- lm(data = merge_data.df, 
+             formula = paste0("log(ruv_desplazamiento_forzado + quantile(ruv_desplazamiento_forzado, .25)^2/quantile(ruv_desplazamiento_forzado, .75)) ~  spraying +", paste(controles, collapse = "+")))
+summary(fe.reg)
+
+final.reg <- lm(data = merge_data.df, 
+          formula = paste0("log(ruv_desplazamiento_forzado + quantile(ruv_desplazamiento_forzado, .25)^2/quantile(ruv_desplazamiento_forzado, .75)) ~  spraying +", paste(controles_fe, collapse = "+")))
+summary(final.reg)
+
+m1coeffs_std <- data.frame(summary(fe.reg)$coefficients)
+coi_indices <- which(!startsWith(row.names(m1coeffs_std), 'codmpio'))
+m1coeffs_std[coi_indices,]
+
+m1coeffs_cl <- coeftest(fe.reg, vcov = vcovCL, cluster = ~codmpio)
+m1coeffs_cl[coi_indices,]
+(m1cis <- coefci(fe.reg, parm = coi_indices, vcov = vcovCL,
+                 cluster = ~codmpio, level = 0.90))
+# Clusterizar errores mostrarlo
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+##   3. Instrumental Variables Regression                                                                   ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+windIV.reg <- ivreg(data = merge_data.df,
+                    formula = paste0("log(ruv_desplazamiento_forzado + quantile(ruv_desplazamiento_forzado, .25)^2/quantile(ruv_desplazamiento_forzado, .75)) ~ spraying | windSpeedFLDAS + windIV05RMBOS +", paste(controles_fe, collapse = "+")))
+summary(windIV.reg)
+
+m1coeffs_std <- data.frame(summary(windIV.reg)$coefficients)
+coi_indices <- which(!startsWith(row.names(m1coeffs_std), 'codmpio'))
+m1coeffs_std[coi_indices,]
+
+m1coeffs_cl <- coeftest(windIV.reg, vcov = vcovCL, cluster = ~codmpio)
+m1coeffs_cl[coi_indices,]
+(m1cis <- coefci(windIV.reg, parm = coi_indices, vcov = vcovCL,
+                 cluster = ~codmpio, level = 0.90))
+
+# Robustness Checks
+
+# First Stage
+
+firstStage.reg <- lm (data = merge_data.df,
+                      formula = paste0("spraying ~ windIV05RMBOS +", paste(controles_fe, collapse = "+")))
+summary(firstStage.reg)
+
+# Añadir acumulado
